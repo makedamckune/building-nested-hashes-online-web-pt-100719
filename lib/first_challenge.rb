13bccfcfbb7b5b761f41f0c2a85b2040c75ef1cf
Makeda P Mckune <makedamckune@gmail.com>
2019-10-14 14:39:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,15 @@
-def first_challenge
-   epic_tragedy = {}
-end
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
+ 
